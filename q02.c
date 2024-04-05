92f61988b812b0503f79a16cca568f7b32c5752d
daenaram <157431231+daenaram@users.noreply.github.com>
2024-04-05 21:28:35 +1300

    Create q02.c

---
+++ q02.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    
+    char secretWord[128];
+    printf("Enter a secret word:\n");
+    scanf("%s", secretWord);
+    
+    int i = 0;
+    while (secretWord[i] != '\0') {
+        secretWord[i]--;
+        
+        if (secretWord[i] < 'A') {
+            secretWord[i] = 'Z';
+        } else if (secretWord[i] < 'a' && secretWord[i] > 'Z') {
+            secretWord[i] = 'z';
+        }
+        
+        i++;
+    }
+    
+    printf("Decrypted: %s\n", secretWord);
+    
+    return 0;
+}
+
+/*
+Enter a secret word:
+Decrypted: hello
+*/
