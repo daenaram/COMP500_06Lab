bf72f393fa1e8f40aa462acb92217b165d724d0b
daenaram <157431231+daenaram@users.noreply.github.com>
2024-04-05 23:18:56 +1300

    Create q21.c

---
+++ q21.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+
+int main(void){
+    
+    int num;
+    
+    printf("Input a number between 0 and 9:");
+    scanf("%d", &num);
+    
+    switch (num){
+        
+        case 0:
+        {
+            printf("\nzero");
+            break;
+        } 
+        
+        case 1:
+        {
+            printf("\none");
+            break;
+        }
+        case 2:
+        {
+            printf("\ntwo");
+            break;
+        } 
+        case 3:
+        {
+            printf("\nthree");
+            break;
+        } 
+        case 4:
+        {
+            printf("\nfour");
+            break;
+        } 
+        case 5:
+        {
+            printf("\nfive");
+            break;
+        } 
+        case 6:
+        {
+            printf("\nsix");
+            break;
+        } 
+        case 7:
+        {
+            printf("\nseven");
+            break;
+        } 
+        case 8:
+        {
+            printf("\neight");
+            break;
+        } 
+        case 9:
+        {
+            printf("\nnine");
+            break;
+        } 
+        
+        default:
+            {
+                printf("\nInvalid input!");
+                break;
+            }
+    }
+    
+    return 0;
+}#include <stdio.h>
+
+int main(void){
+    
+    int num;
+    
+    printf("Input a number between 0 and 9:");
+    scanf("%d", &num);
+    
+    switch (num){
+        
+        case 0:
+        {
+            printf("\nzero");
+            break;
+        } 
+        
+        case 1:
+        {
+            printf("\none");
+            break;
+        }
+        case 2:
+        {
+            printf("\ntwo");
+            break;
+        } 
+        case 3:
+        {
+            printf("\nthree");
+            break;
+        } 
+        case 4:
+        {
+            printf("\nfour");
+            break;
+        } 
+        case 5:
+        {
+            printf("\nfive");
+            break;
+        } 
+        case 6:
+        {
+            printf("\nsix");
+            break;
+        } 
+        case 7:
+        {
+            printf("\nseven");
+            break;
+        } 
+        case 8:
+        {
+            printf("\neight");
+            break;
+        } 
+        case 9:
+        {
+            printf("\nnine");
+            break;
+        } 
+        
+        default:
+            {
+                printf("\nInvalid input!");
+                break;
+            }
+    }
+    
+    return 0;
+}
+
+// ugh fck this sht 
+// result 
+/*
+Input a number between 0 and 9:
+eight
+*/
