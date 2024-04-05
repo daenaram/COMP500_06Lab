cb423fe4cbca6c44358979b1d1e7dd6d52f9cd8a
daenaram <157431231+daenaram@users.noreply.github.com>
2024-04-05 21:56:35 +1300

    Create q12.c

---
+++ q12.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+    
+    int start, end, step;
+    
+    printf("Count down start?\n");
+    scanf("%d", &start);
+    
+    printf("Count down end?\n");
+    scanf("%d", &end);
+    
+    printf("Step size?\n");
+    scanf("%d", &step);
+    
+    for (int i = start; i >= end; i -= step){
+        printf("%d", i);
+        
+        if (i > end){
+            printf(", ");
+        }
+    }
+    
+    
+    return 0;
+}
+
+// result 
+/* 
+Count down start?
+Count down end?
+Step size?
+6, 5, 4, 3, 2, 1
+*/ 
