90da8f2c7d66d933651c9ee2b9c4a380d8f61ff7
daenaram <157431231+daenaram@users.noreply.github.com>
2024-04-05 16:40:29 +1300

    Create q04.c

---
+++ q04.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    
+    int number;
+    
+    printf("Please enter a whole number:\n");
+    scanf("%d", &number);
+    if (number % 2 == 0)
+    {
+        printf("%d is even.", number);
+    }
+    else {
+        printf("%d is odd.", number);
+    }
+}
